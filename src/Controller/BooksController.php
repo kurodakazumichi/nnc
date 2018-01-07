8ae7f66db4cb2b0447b3839463db81078c68b09c
kazumichi kuroda <kazumichi96@gmail.com>
2018-01-07 16:31:17 +0900

    Added Books Related Links.

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -4,111 +4,123 @@ namespace App\Controller;
 use App\Controller\AppController;
 
 /**
- * Books Controller
- *
- * @property \App\Model\Table\BooksTable $Books
- *
- * @method \App\Model\Entity\Book[] paginate($object = null, array $settings = [])
- */
+* Books Controller
+*
+* @property \App\Model\Table\BooksTable $Books
+*
+* @method \App\Model\Entity\Book[] paginate($object = null, array $settings = [])
+*/
 class BooksController extends AppController
 {
+  /**
+  * before action method
+  */
+  public function beforeFilter($event)
+  {
+    parent::beforeFilter($event);
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
-    public function index()
-    {
-        $this->paginate = [
-            'contain' => ['Categories']
-        ];
-        $books = $this->paginate($this->Books);
+    $this->addRelatedLink(['Books'    , 'index' ], 'List Books');
+    $this->addRelatedLink(['Books'    , 'add'   ], 'New Book');
+    $this->addRelatedLink(['Sections' , 'index' ], 'List Sections');
+    $this->addRelatedLink(['Sections' , 'add'   ], 'New Section');
+  }
+  
+  /**
+  * Index method
+  *
+  * @return \Cake\Http\Response|void
+  */
+  public function index()
+  {
+    $this->paginate = [
+      'contain' => ['Categories']
+    ];
+    $books = $this->paginate($this->Books);
 
-        $this->set(compact('books'));
-    }
+    $this->set(compact('books'));
+  }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Book id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $book = $this->Books->get($id, [
-            'contain' => ['Categories', 'Sections']
-        ]);
+  /**
+  * View method
+  *
+  * @param string|null $id Book id.
+  * @return \Cake\Http\Response|void
+  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+  */
+  public function view($id = null)
+  {
+    $book = $this->Books->get($id, [
+      'contain' => ['Categories', 'Sections']
+    ]);
 
-        $this->set('book', $book);
-    }
+    $this->set('book', $book);
+  }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $book = $this->Books->newEntity();
-        if ($this->request->is('post')) {
-            $book = $this->Books->patchEntity($book, $this->request->getData());
-            if ($this->Books->save($book)) {
-                $this->Flash->success(__('The book has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The book could not be saved. Please, try again.'));
-        }
-        $categories = $this->Books->Categories->find('list', ['limit' => 200]);
-        $sections = $this->Books->Sections->find('list', ['limit' => 200]);
-        $this->set(compact('book', 'categories', 'sections'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Book id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $book = $this->Books->get($id, [
-            'contain' => ['Sections']
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $book = $this->Books->patchEntity($book, $this->request->getData());
-            if ($this->Books->save($book)) {
-                $this->Flash->success(__('The book has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The book could not be saved. Please, try again.'));
-        }
-        $categories = $this->Books->Categories->find('list', ['limit' => 200]);
-        $sections = $this->Books->Sections->find('list', ['limit' => 200]);
-        $this->set(compact('book', 'categories', 'sections'));
-    }
-
-    /**
-     * Delete method
-     *
-     * @param string|null $id Book id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
-        $this->request->allowMethod(['post', 'delete']);
-        $book = $this->Books->get($id);
-        if ($this->Books->delete($book)) {
-            $this->Flash->success(__('The book has been deleted.'));
-        } else {
-            $this->Flash->error(__('The book could not be deleted. Please, try again.'));
-        }
+  /**
+  * Add method
+  *
+  * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+  */
+  public function add()
+  {
+    $book = $this->Books->newEntity();
+    if ($this->request->is('post')) {
+      $book = $this->Books->patchEntity($book, $this->request->getData());
+      if ($this->Books->save($book)) {
+        $this->Flash->success(__('The book has been saved.'));
 
         return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('The book could not be saved. Please, try again.'));
     }
+    $categories = $this->Books->Categories->find('list', ['limit' => 200]);
+    $sections = $this->Books->Sections->find('list', ['limit' => 200]);
+    $this->set(compact('book', 'categories', 'sections'));
+  }
+
+  /**
+  * Edit method
+  *
+  * @param string|null $id Book id.
+  * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+  * @throws \Cake\Network\Exception\NotFoundException When record not found.
+  */
+  public function edit($id = null)
+  {
+    $book = $this->Books->get($id, [
+      'contain' => ['Sections']
+    ]);
+    if ($this->request->is(['patch', 'post', 'put'])) {
+      $book = $this->Books->patchEntity($book, $this->request->getData());
+      if ($this->Books->save($book)) {
+        $this->Flash->success(__('The book has been saved.'));
+
+        return $this->redirect(['action' => 'index']);
+      }
+      $this->Flash->error(__('The book could not be saved. Please, try again.'));
+    }
+    $categories = $this->Books->Categories->find('list', ['limit' => 200]);
+    $sections = $this->Books->Sections->find('list', ['limit' => 200]);
+    $this->set(compact('book', 'categories', 'sections'));
+  }
+
+  /**
+  * Delete method
+  *
+  * @param string|null $id Book id.
+  * @return \Cake\Http\Response|null Redirects to index.
+  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+  */
+  public function delete($id = null)
+  {
+    $this->request->allowMethod(['post', 'delete']);
+    $book = $this->Books->get($id);
+    if ($this->Books->delete($book)) {
+      $this->Flash->success(__('The book has been deleted.'));
+    } else {
+      $this->Flash->error(__('The book could not be deleted. Please, try again.'));
+    }
+
+    return $this->redirect(['action' => 'index']);
+  }
 }
